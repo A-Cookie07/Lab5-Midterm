dc2d1c1529917aa307036eec36bc3927c8f15106
netman <zach0311@comcast.net>
2026-02-18 18:37:11 -0700

    implemented commit and push to NMgithub

--- NMgithub.py
+++ NMgithub.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
-from git import Repo
+from git import Repo, RemoteProgress
+local_dir = "./"
+repo = Repo(local_dir)
 
 def main():
-    print("This is working!")
+    last_commit = repo.head.commit
+    print(f"last commit was {last_commit.message}")
+    print(f"this_repo remotes {len(repo.remotes)}")
+
+    repo.git.add(all=True)
+    message = input("input the commit message: \n")
+    repo.index.commit(f"{message}")
+    repo.remote("origin").push()
 
 if __name__ == "__main__":
     main()
